c1d7274eb23d5a8810246dc943bde4c7b7ed588d
Thomas Homsy <thomas.homsy@sky.uk>
2020-12-11 17:45:03 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import Discord from "discord.js";
+import dotenv from "dotenv";
+dotenv.config();
+const client = new Discord.Client();
+
+
+client.on("message", function (message) {
+  console.log(message.author + ": " + message.content);
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client
+  .login(process.env.BOT_TOKEN)
+
+//   .catch((error) => {
+//     console.log(error.message);
+//   });
