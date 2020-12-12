fe3a3d702f94629752965f44623a5d633baa8b41
Thomas Homsy <thomas.homsy@sky.uk>
2020-12-12 11:13:40 +0000

    Stuck it all inside 1 file

--- index.js
+++ index.js
@@ -3,6 +3,34 @@ const Discord = require("discord.js");
 require("dotenv").config();
 const client = new Discord.Client();
 
+class Database {
+  constructor() {
+    this.mysql = require("mysql");
+
+    this.connection = mysql.connection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+    });
+
+    this.connection.connect(function (error) {
+      console.log(error);
+    });
+  }
+
+  setUpTables() {
+    this.connection.query("DROP DATABASE busr;").catch((err) => {
+      console.log(err.message);
+    });
+    this.connection.query(
+      "CREATE TABLE `busr_members` ( `discord_id` INT(64) unsigned NOT NULL, `discord_name` VARCHAR(64) DEFAULT '', `isAdmin` BOOLEAN(1) NOT NULL DEFAULT '0', `university` VARCHAR(40) DEFAULT '', PRIMARY KEY (`discord_id`) );"
+    );
+    this.connection.query(
+      "INSERT INTO `busr_members` VALUES(188720056404803586, `Respects#3394`, 1, `University Of Kent`)"
+    );
+  }
+}
+
 const db = new Database();
 db.setUpTables();
 new client.on("message", function (message) {
