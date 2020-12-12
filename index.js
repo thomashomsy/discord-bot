b31872a8f4cdcd23c578277e1ba6ac6f202a945e
Thomas Homsy <thomas.homsy@sky.uk>
2020-12-12 11:14:43 +0000

    added this

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ class Database {
   constructor() {
     this.mysql = require("mysql");
 
-    this.connection = mysql.connection({
+    this.connection = this.mysql.connection({
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
