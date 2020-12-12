02503e5b5328babbba491805597b53e6d67378bc
Thomas Homsy <thomas.homsy@sky.uk>
2020-12-12 20:20:54 +0000

    Setup tables on Ubuntu Droplet

--- helpers/database.js
+++ helpers/database.js
@@ -2,12 +2,12 @@
 
 class Database {
   constructor() {
-    this.mysql = require("mysql");
-
-    this.connection = mysql.connection({
+    this.connection = mysql.createConnection({
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
+      database: "busr",
+      insecureAuth: true,
     });
 
     this.connection.connect(function (error) {
@@ -16,14 +16,17 @@ class Database {
   }
 
   setUpTables() {
-    this.connection.query("DROP DATABASE busr;").catch((err) => {
-      console.log(err.message);
-    });
+    this.connection.query("DROP DATABASE busr;");
+    this.connection.query("CREATE DATABASE busr;");
+    this.connection.query("USE busr;");
     this.connection.query(
-      "CREATE TABLE `busr_members` ( `discord_id` INT(64) unsigned NOT NULL, `discord_name` VARCHAR(64) DEFAULT '', `isAdmin` BOOLEAN(1) NOT NULL DEFAULT '0', `university` VARCHAR(40) DEFAULT '', PRIMARY KEY (`discord_id`) );"
+      "CREATE TABLE `busr_members` ( `discord_id` INT(64) unsigned NOT NULL, `discord_name` VARCHAR(64) DEFAULT '', `isAdmin` BOOLEAN NOT NULL DEFAULT 0, `university` VARCHAR(40) DEFAULT '', PRIMARY KEY (`discord_id`) );"
     );
     this.connection.query(
-      "INSERT INTO `busr_members` VALUES(188720056404803586, `Respects#3394`, 1, `University Of Kent`)"
+      "INSERT INTO `busr_members` VALUES(188720056404803586, `Respects#3394`, 1, `University Of Kent`)",
+      function (err, result) {
+        console.log(result);
+      }
     );
   }
 }
--- index.js
+++ index.js
@@ -27,16 +27,13 @@ class Database {
       "CREATE TABLE `busr_members` ( `discord_id` INT(64) unsigned NOT NULL, `discord_name` VARCHAR(64) DEFAULT '', `isAdmin` BOOLEAN NOT NULL DEFAULT 0, `university` VARCHAR(40) DEFAULT '', PRIMARY KEY (`discord_id`) );"
     );
     this.connection.query(
-      "INSERT INTO `busr_members` VALUES(188720056404803586, `Respects#3394`, 1, `University Of Kent`)",
-      function (err, result) {
-        console.log(result);
-      }
+      "INSERT INTO `busr_members` VALUES(188720056404803586, `Respects#3394`, 1, `University Of Kent`)"
     );
   }
 }
 
 const db = new Database();
-db.setUpTables();
+//db.setUpTables();
 new client.on("message", function (message) {
   console.log(
     message.author + " - " + message.author.tag + ": " + message.content
