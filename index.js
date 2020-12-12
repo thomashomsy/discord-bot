28eb7295ee261f339101a8a358e8f9505ec7fd47
Thomas Homsy <thomas.homsy@sky.uk>
2020-12-12 11:21:08 +0000

    Small Fix

--- index.js
+++ index.js
@@ -18,14 +18,16 @@ class Database {
   }
 
   setUpTables() {
-    this.connection.query("DROP DATABASE busr;").catch((err) => {
-      console.log(err.message);
-    });
+    this.connection.query("DROP DATABASE busr;");
+    this.connection.query("CREATE DATABASE busr;");
     this.connection.query(
       "CREATE TABLE `busr_members` ( `discord_id` INT(64) unsigned NOT NULL, `discord_name` VARCHAR(64) DEFAULT '', `isAdmin` BOOLEAN(1) NOT NULL DEFAULT '0', `university` VARCHAR(40) DEFAULT '', PRIMARY KEY (`discord_id`) );"
     );
     this.connection.query(
-      "INSERT INTO `busr_members` VALUES(188720056404803586, `Respects#3394`, 1, `University Of Kent`)"
+      "INSERT INTO `busr_members` VALUES(188720056404803586, `Respects#3394`, 1, `University Of Kent`)",
+      function (err, result) {
+        console.log(result);
+      }
     );
   }
 }
