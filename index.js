037f1404277fa3bc6af681ea481cefb531f13aaa
Thomas Homsy <thomas.homsy@sky.uk>
2020-12-12 20:17:39 +0000

    Fixed SQL Statement

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ class Database {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
+      database: "busr",
+      insecureAuth: true,
     });
 
     this.connection.connect(function (error) {
@@ -21,7 +23,7 @@ class Database {
     this.connection.query("DROP DATABASE busr;");
     this.connection.query("CREATE DATABASE busr;");
     this.connection.query(
-      "CREATE TABLE `busr_members` ( `discord_id` INT(64) unsigned NOT NULL, `discord_name` VARCHAR(64) DEFAULT '', `isAdmin` BOOLEAN(1) NOT NULL DEFAULT '0', `university` VARCHAR(40) DEFAULT '', PRIMARY KEY (`discord_id`) );"
+      "CREATE TABLE `busr_members` ( `discord_id` INT(64) unsigned NOT NULL, `discord_name` VARCHAR(64) DEFAULT '', `isAdmin` BOOLEAN NOT NULL DEFAULT 0, `university` VARCHAR(40) DEFAULT '', PRIMARY KEY (`discord_id`) );"
     );
     this.connection.query(
       "INSERT INTO `busr_members` VALUES(188720056404803586, `Respects#3394`, 1, `University Of Kent`)",
